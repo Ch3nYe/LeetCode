4cc39ec57fa040bd260b857af2f970d182abf122
Ch3nYe <sud0su@qq.com>
2022-03-10 17:31:06 +0800

    update

--- src/problem_0164_maximum_gap/bukets_and_pigeonhole_v2.rs
+++ src/problem_0164_maximum_gap/bukets_and_pigeonhole_v2.rs
@@ -2,7 +2,7 @@ pub struct Solution;
 
 impl Solution {
     pub fn maximum_gap(nums: Vec<i32>) -> i32 {
-        let len = nums.len() as i32;
+        let len = nums.len() as usize;
         if len < 2 {
             return 0
         }
@@ -12,11 +12,12 @@ impl Solution {
             *nums.iter().max().unwrap(),
         );
 
-        let avrGap = ((max - min)/(len - 1)); // 2
-        let size = len;
-        let mut buckets = vec![None; size as usize];
+        let avrGap = (max - min) as f32/(len - 1) as f32;
+        let mut buckets = vec![None; len];
         nums.into_iter().for_each(|val| {
-            let b = &mut buckets[((val - min)/avrGap) as usize];
+            let index = ((val - min) as f32/avrGap) as usize;
+            let b = &mut buckets[index];
+            // init buckets
             if let Some((x, y)) = b {
                 *x = val.min(*x);
                 *y = val.max(*y);
