b6ddb4fb5c55020a107c56d4cf1e2bca3bd832b1
Ch3nYe <sud0su@qq.com>
2022-03-22 23:32:52 +0800

    update

--- src/problem_0680_valid_palindrome_ii/double_pointer.rs
+++ src/problem_0680_valid_palindrome_ii/double_pointer.rs
@@ -1,12 +1,38 @@
+use std::iter::zip;
+
 pub struct Solution;
 
 impl Solution {
     pub fn valid_palindrome(s: String) -> bool {
-
+        let mut fors: Vec<char> = s.chars().collect();
+        let mut revs: Vec<char> = s.chars().rev().collect();
+        if fors==revs {
+            return true;
+        }
+        for i in 0..s.len() {
+            if fors[i] == revs[i]{
+                continue;
+            }
+            let mut tmp = revs[..s.len()-i-1].to_vec();
+            let mut tmp2 = fors[i..s.len()-i-1].to_vec();
+            tmp2.append(&mut fors[s.len() - i..].to_vec());
+            if fors[i+1..] == tmp {
+                return true;
+            } else if revs[i+1..] == tmp2 {
+                return true;
+            } else {
+                return false
+            }
+        }
         true
     }
 }
-
+// acbefeca
+// acefebca
+// acefebca fors
+// acbefeca rev
+// abca fors
+// acba revs
 // ------------------------------------------------------ snip ------------------------------------------------------ //
 
 impl super::Solution for Solution {
