62dec9ba243b208a44b3d25fa59628f3cfb27996
Ch3nYe <sud0su@qq.com>
2022-03-25 14:21:22 +0800

    update

--- src/problem_1496_path_crossing/hashset.rs
+++ src/problem_1496_path_crossing/hashset.rs
@@ -1,8 +1,27 @@
+use std::collections::HashSet;
+
 pub struct Solution;
 
 impl Solution {
     pub fn is_path_crossing(path: String) -> bool {
+        let mut map= HashSet::new();
+        let mut path = path.chars();
+        let (mut x, mut y) = (0,0);
+        map.insert((x,y));
 
+        while let Some(i) = path.next() {
+            match i {
+                'N' =>x+=1,
+                'E' =>y+=1,
+                'S' =>x-=1,
+                'W' =>y-=1,
+                _ => ()
+            };
+            if map.contains(&(x, y)) {
+                return true;
+            }
+            map.insert((x, y));
+        }
         false
     }
 }
