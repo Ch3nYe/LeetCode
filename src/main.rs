8f6f1fb5ef195084379e72100e935cec34fd96e7
Ch3nYe <sud0su@qq.com>
2022-03-26 23:07:48 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-pub mod data_structures;
-pub mod test_utilities;
+mod data_structures;
+mod test_utilities;
 // 做完一个就注释一个，cargo test 的时候就不会test到注释掉的
-// pub mod problem_0164_maximum_gap;
-// pub mod problem_xxxx_count_integer_partitions;
-// pub mod problem_0373_find_k_pairs_with_smallest_sums;
-// pub mod problem_0933_number_of_recent_calls;
-// pub mod problem_1498_number_of_subsequences_that_satisfy_the_given_sum_condition;
-// pub mod problem_0873_length_of_longest_fibonacci_subsequence;
-// pub mod problem_0372_super_pow;
-// pub mod problem_xxxx_integer_factorization_problem;
-// pub mod problem_1941_check_if_all_characters_have_equal_number_of_occurrences;
-// pub mod problem_xxxx_k_radius_subarray_averages;
-// pub mod problem_0680_valid_palindrome_ii;
-// pub mod problem_1967_number_of_strings_that_appear_as_substrings_in_word;
-pub mod problem_1496_path_crossing;
+// mod problem_0164_maximum_gap;
+// mod problem_xxxx_count_integer_partitions;
+// mod problem_0373_find_k_pairs_with_smallest_sums;
+// mod problem_0933_number_of_recent_calls;
+// mod problem_1498_number_of_subsequences_that_satisfy_the_given_sum_condition;
+// mod problem_0873_length_of_longest_fibonacci_subsequence;
+// mod problem_0372_super_pow;
+// mod problem_xxxx_integer_factorization_problem;
+// mod problem_1941_check_if_all_characters_have_equal_number_of_occurrences;
+// mod problem_xxxx_k_radius_subarray_averages;
+// mod problem_0680_valid_palindrome_ii;
+// mod problem_1967_number_of_strings_that_appear_as_substrings_in_word;
+// mod problem_1496_path_crossing;
+mod problem_1124_longest_well_performing_interval;
 
 fn main() {
     // let res = problem_0873_length_of_longest_fibonacci_subsequence::dp_binary_search::Solution::len_longest_fib_subseq(
---
+++ src/problem_1124_longest_well_performing_interval/mod.rs
@@ -0,0 +1,29 @@
+/*
+@author: Ch3nYe
+@license: GPL
+@contact: sud0su@qq.com
+@date: 2022/3/26 22:51
+*/
+
+pub mod dynamic;
+pub mod presum_monotonous_stack;
+
+pub trait Solution {
+    fn longest_wpi(hours: Vec<i32>) -> i32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (&[9,9,6,0,6,6,9] as &[_], 3),
+            (&[6,6,6] as &[_], 0),
+        ];
+
+        for (hours, expected) in test_cases {
+            assert_eq!(S::longest_wpi(hours.to_vec()), expected);
+        }
+    }
+}
