0543f54685f0f10788d51e611f454195bf7e77e1
Ch3nYe <sud0su@qq.com>
2022-04-07 14:20:51 +0800

    update

---
+++ src/problem_0796_rotate_string/dynamic.rs
@@ -0,0 +1,23 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn rotate_string(s: String, goal: String) -> bool {
+        true
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn min_score_triangulation(values: Vec<i32>) -> i32 {
+        Self::min_score_triangulation(values)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
---
+++ src/problem_0796_rotate_string/mod.rs
@@ -0,0 +1,27 @@
+/*
+@author: Ch3nYe
+@license: GPL
+@contact: sud0su@qq.com
+@date: 2022/4/7 14:10
+*/
+pub mod dynamic;
+// https://leetcode-cn.com/problems/rotate-string/
+pub trait Solution {
+    fn rotate_string(s: String, goal: String) -> bool;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            ("abcde", "cdeab", true),
+            ("abcde", "abced", false),
+        ];
+
+        for (s, goal, expected) in test_cases {
+            assert_eq!(S::rotate_string(s.to_string(),goal), expected);
+        }
+    }
+}
