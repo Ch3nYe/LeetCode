c81b4d1d7aaa3e044a3d71d6471d2e77fd912d6f
Ch3nYe <sud0su@qq.com>
2022-04-22 23:22:24 +0800

    update

---
+++ src/problem_0401_binary_watch/mod.rs
@@ -0,0 +1,28 @@
+/*
+@author: Ch3nYe
+@license: GPL
+@contact: sud0su@qq.com
+@date: 2022/4/22 23:19
+*/
+
+pub mod backtract;
+// https://leetcode-cn.com/problems/binary-watch/
+pub trait Solution {
+    fn read_binary_watch(turned_on: i32) -> Vec<String>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    pub fn run<S: Solution>() {
+        let test_cases = [
+            (1, &["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"] as &[_]),
+            (1, &[100000]),
+        ];
+
+        for (turned_on, expected) in test_cases {
+            assert_eq!(S::read_binary_watch(turned_on), expected);
+        }
+    }
+}
