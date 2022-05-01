a304200c18e17761e5d4cccf3246495e1a413817
Ch3nYe <sud0su@qq.com>
2022-05-01 23:49:36 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,8 @@ mod test_utilities;
 // mod problem_0052_solve_n_queens_ii;
 // mod problem_0401_binary_watch;
 // mod problem_0089_gray_code;
-mod problem_xxxx_continuous_postage;
+// mod problem_xxxx_continuous_postage; // not AC
+mod problem_0257_binary_tree_paths;
 
 fn main() {
     // let res = problem_0873_length_of_longest_fibonacci_subsequence::dp_binary_search::Solution::len_longest_fib_subseq(
