1f1a2af7faab709d19ef3c7292a6ff4dc90850ca
Ch3nYe <sud0su@qq.com>
2022-05-08 14:40:02 +0800

    update

--- src/problem_0112_has_path_sum/bfs.rs
+++ src/problem_0112_has_path_sum/bfs.rs
@@ -2,16 +2,31 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use crate::data_structures::TreeNode;
 
-pub struct Solution {
-    sum: i32
-};
+pub struct Solution;
 
 impl Solution {
-    let mut sum
-    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+    pub fn dfs(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32, curr_sum: i32, stack: &mut Vec<Rc<RefCell<TreeNode>>>) -> bool {
         if root.is_none() {
             return false;
         }
+        if let Some(mut node) = root {
+            let (val, left, right) = {
+                let node_ref = node.borrow();
+
+                (node_ref.val, node_ref.left.clone(), node_ref.right.clone())
+            };
+            if val + curr_sum == target_sum {
+                return true;
+            } else {
+
+            }
+        }
+        false
+
+    }
+    pub fn has_path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> bool {
+        let mut stack: Vec<Rc<RefCell<TreeNode>>> = Vev::new();
+        Self::dfs(root, target_sum, 0, &mut stack)
 
     }
 }
