00a5febf6dc618fb20f3eb6866a34728b7fb1843
Ch3nYe <sud0su@qq.com>
2022-05-13 10:46:55 +0800

    update

--- src/problem_0107_binary_tree_level_order_traversal_ii/bfs.rs
+++ src/problem_0107_binary_tree_level_order_traversal_ii/bfs.rs
@@ -1,14 +1,44 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::VecDeque;
+use crate::data_structures::TreeNode;
 
 pub struct Solution;
 
 impl Solution {
     pub fn level_order_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        let mut result = Vec::new();
 
+        if let Some(node) = root {
+            let mut queue = VecDeque::from(vec![node]);
+
+            loop {
+                let mut level = Vec::with_capacity(queue.len());
+
+                for _ in 0..queue.len() {
+                    let node = queue.pop_front().unwrap();
+                    let node = node.borrow();
+
+                    level.push(node.val);
+
+                    match (node.left.clone(), node.right.clone()) {
+                        (Some(left),Some(right)) => {queue.push_back(left);queue.push_back(right);},
+                        (Some(child),None)|(None,Some(child)) => {queue.push_back(child);},
+                        _ => {}
+                    }
+                }
+
+                result.push(level);
+
+                if queue.is_empty() {
+                    break;
+                }
+            }
+        }
+        result.reverse();
+        result
     }
 }
-
 // ------------------------------------------------------ snip ------------------------------------------------------ //
 
 impl super::Solution for Solution {
